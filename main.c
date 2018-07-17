58da7073a59aff3c8c57007340cde779a604a0f1
mikeqoo1 <mikeqoo123@gmail.com>
2018-07-17 20:16:54 +0800

    實作,測試

--- main.c
+++ main.c
@@ -104,13 +104,24 @@ int main()
 
 //----csplit
 	printf("----csplit分隔----\n");
-	char str[] = "hello@nworld@1024@QAQ@789555@127WWW@n@888888";
-	char *ans[100]; //這邊未來可以動態設定,再做優化
-	size_t size = strsplit(str, ans, "@");
 	int z;
-	printf("原本的str===>%s\n",str);
-	for (z = 0; z < size; ++z) {
-		printf("分割後:第%d個:%s\n", z, ans[z]);
+	char str[] = "hello\r\nnworld\r\n1024\r\nQAQ\r\n789555\r\n127WWW\r\nnWWWWWQ\r\n888895ZZ";
+	//判斷剩下的
+	char *ans[100]; //這邊未來可以動態設定,再做優化
+	if(find_the_rest(str,"\r\n")) {
+		printf("代表有剩餘\n");
+		size_t size = strsplit(str, ans, "\r\n");
+		printf("原本的str===>%s\n",str);
+		for (z = 0; z < size; ++z) {
+			printf("分割後:第%d個:%s\n", z, ans[z]);
+		}
+	} else{
+		printf("代表沒有剩餘\n");
+		size_t size = strsplit(str, ans, "\r\n");
+		printf("原本的str===>%s\n",str);
+		for (z = 0; z < size; ++z) {
+			printf("分割後:第%d個:%s\n", z, ans[z]);
+		}
 	}
 	printf("----csplit分隔----\n");
 //----csplit
