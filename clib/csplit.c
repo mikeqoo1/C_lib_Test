6f52cd14cf0983a969f2f218abaa1ea160c109f1
mikeqoo1 <mikeqoo123@gmail.com>
2018-07-26 11:01:40 +0800

    問題#24 切包多處理,當第一次切包時,就是斷掉的情況

--- clib/csplit.c
+++ clib/csplit.c
@@ -6,26 +6,26 @@ static char THE_REST_STR[1024] = {0}; //剩餘字串
 int strsplit (const char *str, char *ans[], const char *delimiter) {
 	char *All_Str = malloc(2048); //剩餘加上現有的str
 	char *ptr = All_Str; //存一個指標指到All_Str的地址,方便free掉
-	sprintf(All_Str, "%s%s", THE_REST_STR, str);
+	sprintf(All_Str, "%s%s", THE_REST_STR, str); //把剩餘字串接起來,剩餘+現在
 	memset(THE_REST_STR, '\0', strlen(THE_REST_STR)); //清空剩餘字串
 	char *cut_str;
-
 	int index = 0;
 	cut_str = strstr(All_Str, delimiter);
-
-	strncpy(ans[index++], All_Str, strlen(All_Str) - strlen(cut_str));
-
+	if (cut_str == NULL) { //代表是剩餘字串,不然早就切了
+		strcpy(ans[index++], All_Str);
+		strcpy(THE_REST_STR, All_Str);
+	} else {
+		strncpy(ans[index++], All_Str, strlen(All_Str) - strlen(cut_str));
+	}
 	while (cut_str != NULL) {
 		cut_str[0]='\0'; //讓上一個找到的符號改成結束字元
 		All_Str = cut_str + strlen(delimiter); //接起來,再繼續找
-		//printf("while loop All_Str = %s\n",All_Str);
 		cut_str = strstr(All_Str, delimiter);
 		if (cut_str == NULL) { //代表是最後一個字串,一定是剩餘字串,不然早就切了
 			strcpy(ans[index++], All_Str);
 			strcpy(THE_REST_STR, All_Str);
 		} else {
 			strncpy(ans[index++], All_Str, strlen(All_Str) - strlen(cut_str));
-			//printf("while cut_str = %s\n while ans[i++] = <%s> %d\n", cut_str, ans[index-1], index);
 		}
 	}
 	free(ptr);
