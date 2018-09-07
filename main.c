991b950af9a504f760bc47b056a813c8a14fa100
seaflower <30303712+mikeqoo1@users.noreply.github.com>
2018-09-07 11:03:04 +0800

    Switchs example  (#36)
    * WIP switchs
    
    * 增加switchs的範例#35

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 #include "map.h"
 #include "cmap.h"
 #include "csplit.h"
+#include "switchs.h"
 
 #ifdef mocha
-int test1() {
+int test1()
+{
 	int i = 4;
 	int j = 4;
 	// void assert(expression);
@@ -15,7 +17,8 @@ int test1() {
 	return 0;
 }
 
-int test2() {
+int test2()
+{
 	int l = 12;
 
 	assert(l == 12)
@@ -25,7 +28,8 @@ int test2() {
 #endif
 
 #ifdef Coo
-struct PokeMon {
+struct PokeMon
+{
 	size_t size;
 	void (*create)(size_t size);
 	int (*delete)(const char *key);
@@ -33,7 +37,8 @@ struct PokeMon {
 	void (*store)(const char *key, void *value);
 };
 
-void PokeMonNew(struct PokeMon *obj,size_t size) {
+void PokeMonNew(struct PokeMon *obj,size_t size)
+{
 	obj-> create = createmap;
 	obj-> delete = delete;
 	obj-> fetch = fetch;
@@ -41,7 +46,8 @@ void PokeMonNew(struct PokeMon *obj,size_t size) {
 	obj-> size = size;
 }
 #endif
-int main() {
+int main(int argc, char **argv)
+{
 
 //----mocha
 #ifdef mocha
@@ -206,5 +212,31 @@ int main() {
 #endif
 //----C Object Oriented
 	printf("Hello, World!\n");
+	char *argvtemp = "TEST";
+	if (argv[1] != NULL) {
+		argvtemp = argv[1];
+	}
+	switchs(argvtemp) {
+		cases("bar")
+			printf("foo or bar (區分大小寫)\n");
+			break;
+		icases("pi")
+			printf("pi or Pi or pI or PI (不分大小寫)\n");
+			break;
+		cases_re("^D.*",0)
+			printf("Something that start with D (有大寫的D開頭都算)\n");
+			break;
+		cases_re("^E.*",REG_ICASE)
+			printf("Something that start with E (大小寫的E,e開頭都算)\n");
+			break;
+		cases("1")
+			printf("1\n");
+		cases("2")
+			printf("2\n");
+			break;
+		defaults
+			printf("TEST\n");
+			break;
+	} switchs_end;
 	return 0;
 }
